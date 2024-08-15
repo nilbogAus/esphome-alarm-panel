e749c6c5ea58a8a7b7470918be20721c2519016a
nilbog@nilbogcave.com <nilbog@nilbogcave.com>
2024-08-16 09:53:48 +1000

    page text align

--- publishYaml.ps1
+++ publishYaml.ps1
@@ -1,4 +1,5 @@
 # Copy all content including subfolder:
 Copy-Item -Path ".\ESPHome\*"  -Destination "H:\" -Recurse -force
 Clear-Host
-'Files copied. Install via ESPHome.' | Write-Host
+$currentTime = Get-Date -format "dd-MMM-yyyy HH:mm:ss"
+Write-Host $currentTime  ' Files copied. Now install via ESPHome.' 
