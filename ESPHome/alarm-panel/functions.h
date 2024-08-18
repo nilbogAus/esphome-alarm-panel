b4b8e6c0a496abc91e7bccea0ecc3279e40320a4
nilbog@nilbogcave.com <nilbog@nilbogcave.com>
2024-08-18 16:34:15 +1000

    test button working

---
+++ ESPHome/alarm-panel/functions.h
@@ -0,0 +1,7 @@
+int get_text_width(esphome::font::Font *font, const char *text)
+{
+    int width, x_offset, baseline, height;
+    
+    font->measure(text, &width, &x_offset, &baseline, &height);
+    return width;
+}
