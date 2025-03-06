f96600c7ced0528f516f121ea992f6bc98e403d8
nilbogAus <48857141+nilbogAus@users.noreply.github.com>
2025-03-06 17:23:22 +1100

    updates for latest ha changes

--- publishYaml.ps1
+++ publishYaml.ps1
@@ -1,4 +1,4 @@
 Clear-Host
-Copy-Item -Path ".\ESPHome\*"  -Destination "H:\" -Recurse -force
+Copy-Item -Path ".\ESPHome\*"  -Destination "H:\ESPHome\" -Recurse -force
 $currentTime = Get-Date -format "dd-MMM-yyyy HH:mm:ss"
 Write-Host $currentTime  ' Files copied. Now install via ESPHome.' 
